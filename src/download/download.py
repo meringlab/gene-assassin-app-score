5711e14b209f2b1704e994676f5a9509b7346d3c
Milan Simonovic <mbsimonovic@gmail.com>
2017-12-08 10:57:28 +0100

    refactor

--- src/download/download.py
+++ src/download/download.py
@@ -6,46 +6,34 @@ import shutil
 from subprocess import call
 import logging
 
-def download_from_url(url_link, downlading_path, species_name, file_category):
-    file_name = url_link.split("/")[-1]
-    full_path = os.path.join(downlading_path, species_name, file_category)
-    destination = os.path.join(full_path, file_name)
+OUTPUT_DIR = "Raw_data_files"
+
+def download_from_url(url_link, dst_dir):
+    file_name = os.path.basename(url_link)
+    destination = os.path.join(dst_dir, file_name)
     logging.info('downloading %s to %s', url_link, destination)
 
-    if not os.path.exists(full_path):
-        try:
-            os.makedirs(full_path)
-        except Exception as e:
-            logging.exception(e)
-            exit("Cannot create directory %s" % downlading_path)
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
 
-    try:
-        file_link = url_link
-        file_handle = urllib2.urlopen(file_link)
-    except Exception as e:
-        logging.exception(e)
-        exit("\t ... URL link does not work %s." % url_link)
-
-    with open(destination, "w") as f:
-        shutil.copyfileobj(file_handle, f)
-
-    file_handle.close()
-
+    with urllib2.urlopen(url_link) as file_handle:
+        with open(destination, "w") as f:
+            shutil.copyfileobj(file_handle, f)
     logging.info('saved at %s', destination)
+
     return destination
 
+
 def download(params):
-    tempfile_path = "output/%s" % params['ensembl_release']
-    # ensembl_version = "v_85"
-    file_category = "Raw_data_files"
+    dst_dir = os.path.join("output" , params['ensembl_release'], params['species_name'], OUTPUT_DIR)
 
-    download_from_url(params['base_url_gtf'], tempfile_path, params['species_name'], file_category)
+    download_from_url(params['base_url_gtf'], dst_dir)
 
-    destination = download_from_url(params['DNA_top_level_fa'], tempfile_path, params['species_name'],  file_category)
+    destination = download_from_url(params['DNA_top_level_fa'], dst_dir)
     # reading gzip is much much slower than uncompressing and reading uncompressed:
     call(["gunzip", destination])
 
-    destination =download_from_url(params['GVF_file'], tempfile_path, params['species_name'], file_category)
+    destination = download_from_url(params['GVF_file'], dst_dir)
     call(["gunzip", destination])
 
 
@@ -56,7 +44,7 @@ if __name__ == "__main__":
     params = json.load(open(sys.argv[1]))
     download(params)
 
-#base_url_gtf_1 = "ftp://ftp.ensembl.org/pub/release-90/gtf/danio_rerio/Danio_rerio.GRCz10.90.gtf.gz"
-#DNA_top_level_fa = "ftp://ftp.ensembl.org/pub/release-90/fasta/danio_rerio/dna/Danio_rerio.GRCz10.dna.toplevel.fa.gz"
-#GVF_file = "ftp://ftp.ensembl.org/pub/release-90/variation/gvf/danio_rerio/Danio_rerio.gvf.gz"
-#ensembl_version = "v_90"
+# base_url_gtf_1 = "ftp://ftp.ensembl.org/pub/release-90/gtf/danio_rerio/Danio_rerio.GRCz10.90.gtf.gz"
+# DNA_top_level_fa = "ftp://ftp.ensembl.org/pub/release-90/fasta/danio_rerio/dna/Danio_rerio.GRCz10.dna.toplevel.fa.gz"
+# GVF_file = "ftp://ftp.ensembl.org/pub/release-90/variation/gvf/danio_rerio/Danio_rerio.gvf.gz"
+# ensembl_version = "v_90"
