82d75fde43a46ec6f640449724f1e8b4821dd376
Milan Simonovic <mbsimonovic@gmail.com>
2017-12-08 11:08:16 +0100

    refactor

--- run.bash
+++ run.bash
@@ -2,10 +2,8 @@
 
 # Step 1. download
 ## Download the raw files from ENSEMBL ....
-python ./src/download.py conf/drerio.json
+python ./src/download/main.py conf/drerio.json
 
-## Parse to create transcript cds info file
-python ./src/download/parse.py conf/drerio.json
 
 # Step 2. create guides info
 python ./src/guides_info/main.py conf/drerio.json
@@ -13,6 +11,5 @@ python ./src/guides_info/main.py conf/drerio.json
 
 # Step 3. Calculate scores for the guides
 ## Uses guides info from the Step 2
-
 python ./src/scores/main.py conf/drerio.json
 
--- src/download/main.py
+++ src/download/main.py
@@ -1,9 +1,39 @@
+
 import os
 import sys
 import json
 import gzip
 import re
 import logging
+from subprocess import call
+
+OUTPUT_DIR = "Raw_data_files"
+
+
+def download_from_url(url_link, dst_dir):
+    logging.info('downloading %s to %s', url_link, dst_dir)
+    file_name = os.path.basename(url_link)
+    destination = os.path.join(dst_dir, file_name)
+
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+
+    call(["rsync", "-av", url_link, dst_dir])
+
+    return destination
+
+
+def download(params):
+    dst_dir = os.path.join("output", params['ensembl_release'], params['species_name'], OUTPUT_DIR)
+
+    download_from_url(params['base_url_gtf'], dst_dir)
+
+    destination = download_from_url(params['DNA_top_level_fa'], dst_dir)
+    # reading gzip is much much slower than uncompressing and reading uncompressed:
+    call(["gunzip", destination])
+
+    destination = download_from_url(params['GVF_file'], dst_dir)
+    call(["gunzip", destination])
 
 
 def parse_gtf(params):
@@ -304,6 +334,9 @@ def make_transcript_cds_info(params):
 if __name__ == '__main__':
     if len(sys.argv) < 2 or not os.path.exists(sys.argv[1]):
         exit('missing config file!')
+
     params = json.load(open(sys.argv[1]))
+
+    download(params)
     parse_gtf(params)
     make_transcript_cds_info(params)
