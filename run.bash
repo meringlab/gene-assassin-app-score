10eabe42d0f4160f617ccd9dfe08569c6d2605c2
Milan Simonovic <mbsimonovic@gmail.com>
2018-03-16 09:13:59 +0100

    refactor

--- run.bash
+++ run.bash
@@ -3,16 +3,20 @@
 source activate ga-scores
 export PYTHONPATH=.:src
 
-# Step 1. download
-## Download the raw files from ENSEMBL ....
-python ./src/download/main.py conf/drerio.json
+for genome in `ls conf`
+do
+
+    # Step 1. download
+    ## Download the raw files from ENSEMBL ....
+    python ./src/download/main.py conf/$genome
 
 
-# Step 2. create guides info
-python ./src/guides_info/main.py conf/drerio.json
+    # Step 2. create guides info
+    python ./src/guides_info/main.py conf/$genome
 
 
-# Step 3. Calculate scores for the guides
-## Uses guides info from the Step 2
-python ./src/scores/main.py conf/drerio.json
+    # Step 3. Calculate scores for the guides
+    ## Uses guides info from the Step 2
+    python ./src/scores/main.py conf/$genome
 
+done
